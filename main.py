4d5f649a1478e373d75343fd6d9f0609a4ca1af0
pauldechorgnat <pdechorgnat@yahoo.fr>
2022-04-07 16:16:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+api = FastAPI(title="Mon API pour les Orange DE")
+
+
+@api.get('/')
+def get_index():
+    return {
+        "message": "Hello world!"
+    }
+
+
+@api.get(
+    '/mon_endpoint',
+    tags=["demo"],
+    responses={
+        200: {
+            "description": "tout a marché comme sur des roulettes"
+        },
+        501: {
+            "description": "tout va mal"
+        }
+    }
+)
+def get_mon_endpoint(nombre_entier: int):
+    """Renvoie un modele de dictionnaire
+    """
+    return {
+        "un_nombre": nombre_entier,
+        "un_dictionnaire": {
+            "une_clef": "une_valeur",
+            "une_autre_clef": "une_autre_valeur"
+        }
+    }
+
+
+@api.post('/mon_premier_post')
+def post_mon_premier_post():
+    return {
+        "hello": "world"
+    }
---
+++ tests/test.py
@@ -0,0 +1,39 @@
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+
+
+def test_get_index():
+    response = requests.get(
+        url=f"{API_URL}/"
+        # url="{}/".format(API_URL)
+    )
+
+    assert response.status_code == 200, response.content
+
+
+def test_get_mon_endpoint():
+    response = requests.get(
+        url=f"{API_URL}/mon_endpoint",
+        params={"nombre_entier": 24}
+        # url="{}/".format(API_URL)
+    )
+
+    assert response.status_code == 200, response.content
+    data = response.json()
+
+    assert data["un_nombre"] == 24
+
+
+def test_post_mon_premier_post():
+    response = requests.post(
+        url=f"{API_URL}/mon_premier_post",
+
+        # url="{}/".format(API_URL)
+    )
+
+    assert response.status_code == 200, response.content
+    data = response.json()
+
+    assert "hello" in data
+    assert data["hello"] == "world"
